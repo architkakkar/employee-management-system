881460a1f0542c3d4c8a67390a082ff2c2bdaafd
Archit Kakkar <education.archit@gmail.com>
2025-02-21 01:21:55 +0530

    feat: logout functionality implemented

--- src/components/others/Header.jsx
+++ src/components/others/Header.jsx
@@ -1,10 +1,19 @@
-const Header = () => {
+const Header = ({ name }) => {
+  const logoutUser = () => {
+    localStorage.removeItem("loggedInUser");
+    window.location.reload();
+  };
+
   return (
     <div className="flex items-center justify-between mb-6">
       <h1 className="text-md">
-        Hello <br /> <span className="text-3xl font-medium">Archit 👋</span>
+        Hello <br /> <span className="text-3xl font-medium">{name} 👋</span>
       </h1>
-      <button className="bg-yellow-400 text-black font-medium px-6 py-2 rounded-md hover:opacity-90">
+      <button
+        type="button"
+        onClick={logoutUser}
+        className="bg-yellow-400 text-black font-medium px-6 py-2 rounded-md hover:opacity-90"
+      >
         Logout
       </button>
     </div>
